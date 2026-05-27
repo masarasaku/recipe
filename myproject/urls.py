37087ce539aff023a8a46a51861abb8845f866fd
masarasaku <ms999msyk@gmail.com>
2026-05-27 20:44:10 +0900

    add home route

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Render deploy success!")
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('exe01/', include('helloapp.urls')),
     path('exe02/', include('bookapp.urls')),
